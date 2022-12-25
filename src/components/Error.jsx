cd578294e6cba4fa7d7b29a1ba823400be583ab5
beyondnaxos <86872427+beyondnaxos@users.noreply.github.com>
2022-12-25 11:44:21 +0100

    Update Comp

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 
 function Error() {
+
   return (
-    <div>Error</div>
+    <div>Error 404</div>
   )
 }
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
 function Home() {
+
   return (
+
+    // Hero Banner 
+    //  List 
     <div>Home</div>
   )
 }
