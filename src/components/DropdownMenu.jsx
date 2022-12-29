e25f6ca3ae154ba6d7d643eacdedbede1d1890c3
beyondnaxos <86872427+beyondnaxos@users.noreply.github.com>
2022-12-30 00:05:34 +0100

    fixed drowdown arrows

--- src/components/DropdownMenu.jsx
+++ src/components/DropdownMenu.jsx
@@ -12,7 +12,7 @@ export const DropdownMenu = (props) => {
     <div className="dropdown-menu">
       <div className="dropdown-menu-title">
         {title}
-        <button onClick={() => setOpen(!open)}>{open ? '-' : '+'}</button>
+        <button className='dropButton' onClick={() => setOpen(!open)}>{open ? <img className='arrow up' src='/assets/arrowLeft.png'/> : <img className='arrow down' src='/assets/arrowLeft.png'/> }</button>
       </div>
       {open ? null : (
         <div className="dropdown-menu-content">
