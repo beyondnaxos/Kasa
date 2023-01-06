91d8790218e0ac7a966ea3e5710e72433c17acd2
beyondnaxos <86872427+beyondnaxos@users.noreply.github.com>
2023-01-06 13:38:13 +0100

    About module OK

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import { v4 as uuidv4 } from 'uuid'
 import AboutDropdown from '../AboutDropdown/AboutDropdown'
 import * as aboutList from '../../data/about.json'
+import styles from './About.module.css'
 
 export default function About() {
   return (
-    <div className="about-container">
-      <header className="hero about-header">
+    <div className={styles.aboutcontainer}>
+      <header className={`${styles.hero} ${styles.aboutheader}`}>
         <img
-          className="hero-img about-image"
+          className={`${styles.heroimg} ${styles.aboutimage}`}
           src={
             window.innerWidth > 768
               ? '/assets/about.png'
