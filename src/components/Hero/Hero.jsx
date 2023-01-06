8a4c51fb0dcc0b5daa3c5deecbf73e55796e0f63
beyondnaxos <86872427+beyondnaxos@users.noreply.github.com>
2023-01-06 12:30:31 +0100

    Hero Module OK

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import styles from './Hero.module.css'
 
 export default function Hero() {
   return (
-    <header className="hero">
-      <img className="hero-img" src="/assets/background.png" alt="" />
-      <div className="background-layer"></div>
-      <div className="hero-title">
+    <header className={styles.hero}>
+      <img className={styles.heroimg} src="/assets/background.png" alt="" />
+      <div className={styles.backgroundlayer}></div>
+      <div className={styles.herotitle}>
         <span>Chez vous,</span>
         <span>partout et ailleurs</span>
       </div>
