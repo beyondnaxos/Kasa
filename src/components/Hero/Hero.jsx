39235a5bbeb4ca425f23874c96d935fdf6648533
beyondnaxos <86872427+beyondnaxos@users.noreply.github.com>
2023-01-09 12:13:34 +0100

    fixed css hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,12 +3,14 @@ import styles from './Hero.module.css'
 
 export default function Hero() {
   return (
-    <header className={styles.hero}>
+    <header className={styles.herocontainer}>
+      <div className={styles.hero}>
       <img className={styles.heroimg} src="/assets/background.png" alt="" />
       <div className={styles.backgroundlayer}></div>
       <div className={styles.herotitle}>
         <span>Chez vous,</span>
         <span>partout et ailleurs</span>
+      </div>
       </div>
     </header>
   )
