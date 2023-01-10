f8c763a7bb1dd7041797e0aae1d79fc2e72d50a3
beyondnaxos <86872427+beyondnaxos@users.noreply.github.com>
2023-01-10 10:06:54 +0100

    underline

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import styles from './NavBar.module.css'
 
 export default function NavBar() {
+
   return (
     <nav className={styles.navbar}>
       <img src="/assets/LOGO.svg" className={styles.logo}></img>
